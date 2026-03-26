2ec5316a8608179e10cf63a478f47634c8d5bf76
Arnaud <bassolearnaud9@gmail.com>
2026-03-26 13:41:39 +0000

    fix: remove type f

--- app/page.tsx
+++ app/page.tsx
@@ -359,7 +359,7 @@ type Project = {
 };
 
   const items: Project[] = [
-    { img: "e-commerce.png",
+    { img: "commerce.png",
       direction: -100,
       nom:"E-commerce",
       definition :"Site e-commerce permettant de vendre des produits en ligne avec gestion du panier, des commandes et des utilisateurs dans une interface intuitive.",
@@ -724,7 +724,7 @@ const refSection = useRef<HTMLDivElement | null>(null);
                   <main className="mb-0 pb-0 w-full flex justify-center md:px-8 py-10 bg-white text-sm md:text-lg lg:text-2xl">
                     <div className="w-full bg-white max-w-8xl rounded md: shadow overflow-x-hidden">
 
-                          {/*les Texte qui apparaît lettre par lettre */}
+                          {/*Hero section */}
 
                             <div className="bg-[#DFDBE5] bg-repeat bg-[url('data:image/svg+xml,%3Csvg%20width%3D%2716%27%20height%3D%2716%27%20viewBox%3D%270%200%2016%2016%27%20xmlns%3D%27http://www.w3.org/2000/svg%27%3E%3Cpath%20d%3D%27M0%200h16v2h-6v6h6v8H8v-6H2v6H0V0zm4%204h2v2H4V4zm8%208h2v2h-2v-2zm-8%200h2v2H4v-2zm8-8h2v2h-2V4z%27%20fill%3D%27%239C92AC%27%20fill-opacity%3D%270.05%27%20fill-rule%3D%27evenodd%27/%3E%3C/svg%3E')] text-center text-sm + md:text-lg + lg:text-2xl rounded py-30 bg-gray-100 p-2" id="home">
                               <div>
@@ -770,7 +770,7 @@ const refSection = useRef<HTMLDivElement | null>(null);
                                   </motion.p>
 
                               </div>
-<div className="max-w-7xl mx-auto px-10 mt-40">
+                                <div className="max-w-7xl mx-auto px-10 mt-40">
                               <div className="grid grid-cols-1 md:grid-cols-2 gap-18 ">
                                 <div className="bg-gray-900 text-white p-6 rounded-2xl">
                                     <h1 className="text-5xl font-bold mb-6">
@@ -872,9 +872,6 @@ const refSection = useRef<HTMLDivElement | null>(null);
                               </div>
                             </div>
                             </div>
-
-                          {/*Hero section */}
-                          
 
                         {/*motion quand tu scroll : image s'affiche et vas vers le haut  */ }
                           <div className="py-30 bg-gray-100" id="Portfolio">
